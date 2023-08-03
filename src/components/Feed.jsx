4911ed3f236305fa7120a0ea5d56d8b4967e9b33
Lucas <lucascomamala@gmail.com>
2023-08-03 02:01:57 -0600

    Feed empty with some text

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,16 +1,25 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import { Sidebar } from '.';
 
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-        Sidebar
+        <Sidebar />
 
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
           Copyright 2023 Lucas Comamala
         </Typography>
       </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: '2' }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: '#fff' }}>
+          New <span style={{ color: '#f31503' }}>Videos</span>
+        </Typography>
+      </Box>
+
+      Videos
     </Stack>
   )
 }
