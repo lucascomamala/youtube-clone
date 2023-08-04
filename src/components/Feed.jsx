94a7003a83df88f1c4b78c2e6e54487967ac3115
Lucas <lucascomamala@gmail.com>
2023-08-04 13:25:08 -0600

    Fetech videos from API

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,27 @@
+// eslint-disable-next-line
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import { Sidebar } from '.';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Sidebar, Videos } from './';
+
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-        <Sidebar />
+        <Sidebar 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
 
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
           Copyright 2023 Lucas Comamala
@@ -15,11 +30,11 @@ const Feed = () => {
 
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: '2' }}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: '#fff' }}>
-          New <span style={{ color: '#f31503' }}>Videos</span>
+          {selectedCategory} <span style={{ color: '#f31503' }}>Videos</span>
         </Typography>
       </Box>
 
-      Videos
+      <Videos videos={videos} />
     </Stack>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,7 @@ import { Stack } from '@mui/material';
 
 import { categories } from '../utils/constants';
 
-const selectedCategory = 'Coding';
-
-const Sidebar = () => (
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direction="row"
     sx={{
@@ -16,6 +14,7 @@ const Sidebar = () => (
     {categories.map((category) => (
       <button
         className="category-btn"
+        onClick={() => setSelectedCategory(category.name)}
         style={{ 
           backgroundColor: category.name === selectedCategory && '#fc1503',
           color: 'white'
